9c505bfc4f60e8bdffb2ca0820a5f001fc15c2cb
markpan07 <markpan07@mail.ru>
2023-11-27 14:08:33 +0300

    Initial commit

---
+++ src/main/java/ru/wazzem/officemoney/OfficeMoneyApplication.java
@@ -0,0 +1,13 @@
+package ru.wazzem.officemoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficeMoneyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/wazzem/officemoney/OfficeMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.wazzem.officemoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeMoneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
